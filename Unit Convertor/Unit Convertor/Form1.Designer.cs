aab5229f6e969ce82df1d1f0de2f9d046fdde84e
Paraaw5434 <122698242+Paraaw5434@users.noreply.github.com>
2025-03-17 14:20:25 +0330

    Add files via upload

---
+++ Unit Convertor/Unit Convertor/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Unit_Convertor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbUnit1 = new System.Windows.Forms.ComboBox();
+            this.cmbUnit2 = new System.Windows.Forms.ComboBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.cmbUnitSelection = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmbUnit1
+            // 
+            this.cmbUnit1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.cmbUnit1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbUnit1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbUnit1.FormattingEnabled = true;
+            this.cmbUnit1.Location = new System.Drawing.Point(236, 111);
+            this.cmbUnit1.Name = "cmbUnit1";
+            this.cmbUnit1.Size = new System.Drawing.Size(93, 21);
+            this.cmbUnit1.TabIndex = 0;
+            this.cmbUnit1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // cmbUnit2
+            // 
+            this.cmbUnit2.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.cmbUnit2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbUnit2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbUnit2.FormattingEnabled = true;
+            this.cmbUnit2.Location = new System.Drawing.Point(235, 71);
+            this.cmbUnit2.Name = "cmbUnit2";
+            this.cmbUnit2.Size = new System.Drawing.Size(93, 21);
+            this.cmbUnit2.TabIndex = 5;
+            this.cmbUnit2.SelectedIndexChanged += new System.EventHandler(this.cmbUnit2_SelectedIndexChanged);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.txtOutput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(66, 69);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(125, 21);
+            this.txtOutput.TabIndex = 6;
+            this.txtOutput.TextChanged += new System.EventHandler(this.txtOutput_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SkyBlue;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(235, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 40);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Switch";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.cmbUnitSelection);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.cmbUnit1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(340, 201);
+            this.panel1.TabIndex = 10;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Black;
+            this.panel4.Controls.Add(this.textBox2);
+            this.panel4.Location = new System.Drawing.Point(66, 110);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(125, 23);
+            this.panel4.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(0, 0);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 21);
+            this.textBox2.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.panel2.Controls.Add(this.txtOutput);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.cmbUnit2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 194);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(340, 199);
+            this.panel2.TabIndex = 11;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Black;
+            this.panel3.Location = new System.Drawing.Point(66, 69);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(125, 23);
+            this.panel3.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(262, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Dark Mode";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // cmbUnitSelection
+            // 
+            this.cmbUnitSelection.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.cmbUnitSelection.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cmbUnitSelection.Items.AddRange(new object[] {
+            "Temperature",
+            "Length",
+            "Mass"});
+            this.cmbUnitSelection.Location = new System.Drawing.Point(104, 42);
+            this.cmbUnitSelection.Name = "cmbUnitSelection";
+            this.cmbUnitSelection.Size = new System.Drawing.Size(121, 21);
+            this.cmbUnitSelection.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(3, 42);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 27);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "Choose Your Unit";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(340, 393);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Unit Convertor";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbUnit1;
+        private System.Windows.Forms.ComboBox cmbUnit2;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cmbUnitSelection;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Unit Convertor/Unit Convertor/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Unit_Convertor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Margin = new Padding(0);
+            panel1.Padding = new Padding(0);
+            cmbUnitSelection.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbUnitSelection.FlatStyle = FlatStyle.Flat;
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbUnitSelection.DrawMode = DrawMode.OwnerDrawFixed;
+            cmbUnitSelection.BackColor = Color.LightSkyBlue;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtOutput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void cmbUnit2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
