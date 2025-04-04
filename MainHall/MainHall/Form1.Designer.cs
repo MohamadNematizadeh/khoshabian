3ff957eb6377ee5894fbfdfc59ff660b24a4078b
Paraaw5434 <122698242+Paraaw5434@users.noreply.github.com>
2025-04-04 18:14:48 +0330

    Add files via upload

---
+++ MainHall/MainHall/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace MainHall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btnRock = new System.Windows.Forms.Button();
+            this.btnUnit = new System.Windows.Forms.Button();
+            this.btnNumber = new System.Windows.Forms.Button();
+            this.btnList = new System.Windows.Forms.Button();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnRedLight = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.btnRedLight);
+            this.panel1.Location = new System.Drawing.Point(12, 66);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(100, 116);
+            this.panel1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(4, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox1.Size = new System.Drawing.Size(91, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "چراغ راهنمایی";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.btnCalc);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Location = new System.Drawing.Point(196, 66);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(100, 116);
+            this.panel2.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.textBox2.Location = new System.Drawing.Point(5, 91);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(91, 20);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.Text = "ماشین حساب";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.textBox5);
+            this.panel3.Controls.Add(this.btnList);
+            this.panel3.Location = new System.Drawing.Point(196, 219);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(100, 116);
+            this.panel3.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.textBox6);
+            this.panel4.Controls.Add(this.btnRock);
+            this.panel4.Location = new System.Drawing.Point(12, 219);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(100, 116);
+            this.panel4.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel5.Controls.Add(this.textBox3);
+            this.panel5.Controls.Add(this.btnUnit);
+            this.panel5.Location = new System.Drawing.Point(196, 369);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(100, 116);
+            this.panel5.TabIndex = 0;
+            // 
+            // panel6
+            // 
+            this.panel6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel6.Controls.Add(this.textBox7);
+            this.panel6.Controls.Add(this.btnNumber);
+            this.panel6.Location = new System.Drawing.Point(12, 369);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(100, 116);
+            this.panel6.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.textBox3.Location = new System.Drawing.Point(5, 90);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(91, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "تبدیل واحد";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnRock
+            // 
+            this.btnRock.BackgroundImage = global::MainHall.Properties.Resources.Rock;
+            this.btnRock.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRock.Location = new System.Drawing.Point(3, 3);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(92, 80);
+            this.btnRock.TabIndex = 6;
+            this.btnRock.UseVisualStyleBackColor = true;
+            // 
+            // btnUnit
+            // 
+            this.btnUnit.BackgroundImage = global::MainHall.Properties.Resources.Unit;
+            this.btnUnit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnUnit.Location = new System.Drawing.Point(3, 3);
+            this.btnUnit.Name = "btnUnit";
+            this.btnUnit.Size = new System.Drawing.Size(92, 80);
+            this.btnUnit.TabIndex = 8;
+            this.btnUnit.UseVisualStyleBackColor = true;
+            // 
+            // btnNumber
+            // 
+            this.btnNumber.BackgroundImage = global::MainHall.Properties.Resources.Number;
+            this.btnNumber.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnNumber.Location = new System.Drawing.Point(3, 3);
+            this.btnNumber.Name = "btnNumber";
+            this.btnNumber.Size = new System.Drawing.Size(92, 80);
+            this.btnNumber.TabIndex = 7;
+            this.btnNumber.UseVisualStyleBackColor = true;
+            // 
+            // btnList
+            // 
+            this.btnList.BackgroundImage = global::MainHall.Properties.Resources.list;
+            this.btnList.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnList.Location = new System.Drawing.Point(3, 3);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(92, 80);
+            this.btnList.TabIndex = 5;
+            this.btnList.UseVisualStyleBackColor = true;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.BackgroundImage = global::MainHall.Properties.Resources.calc;
+            this.btnCalc.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnCalc.Location = new System.Drawing.Point(3, 4);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(92, 80);
+            this.btnCalc.TabIndex = 1;
+            this.btnCalc.UseVisualStyleBackColor = true;
+            // 
+            // btnRedLight
+            // 
+            this.btnRedLight.BackColor = System.Drawing.Color.Transparent;
+            this.btnRedLight.BackgroundImage = global::MainHall.Properties.Resources.TrafficLightpng;
+            this.btnRedLight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRedLight.Location = new System.Drawing.Point(3, 3);
+            this.btnRedLight.Name = "btnRedLight";
+            this.btnRedLight.Size = new System.Drawing.Size(92, 80);
+            this.btnRedLight.TabIndex = 0;
+            this.btnRedLight.UseVisualStyleBackColor = false;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.textBox5.Location = new System.Drawing.Point(3, 89);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(91, 20);
+            this.textBox5.TabIndex = 6;
+            this.textBox5.Text = "مدیریت کارها";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.textBox6.Location = new System.Drawing.Point(4, 89);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(91, 20);
+            this.textBox6.TabIndex = 7;
+            this.textBox6.Text = "سنگ کاغذ قیچی";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.textBox7.Location = new System.Drawing.Point(4, 89);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(91, 20);
+            this.textBox7.TabIndex = 8;
+            this.textBox7.Text = "حدس عدد";
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Font = new System.Drawing.Font("Stencil", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(8, 21);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(299, 25);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "یکی از برنامه های زیر را انتخاب کنید";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 509);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Main Hall";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnRedLight;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button btnUnit;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.Button btnNumber;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
