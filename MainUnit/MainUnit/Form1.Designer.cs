a066df784c8226d38c1ecd0887e7375ef0ff4871
Paraaw5434 <122698242+Paraaw5434@users.noreply.github.com>
2025-04-07 09:51:31 +0330

    Add files via upload

---
+++ MainUnit/MainUnit/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace MainUnit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.btnWeight = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Teal;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(57, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(171, 23);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "Choose your unit";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.UseWaitCursor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Location = new System.Drawing.Point(103, 99);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(83, 100);
+            this.panel1.TabIndex = 11;
+            this.panel1.UseWaitCursor = true;
+            // 
+            // btnHome
+            // 
+            this.btnHome.BackColor = System.Drawing.Color.Teal;
+            this.btnHome.BackgroundImage = global::MainUnit.Properties.Resources._25694;
+            this.btnHome.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnHome.FlatAppearance.BorderSize = 0;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.Location = new System.Drawing.Point(2, 2);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(28, 27);
+            this.btnHome.TabIndex = 12;
+            this.btnHome.UseVisualStyleBackColor = false;
+            // 
+            // btnWeight
+            // 
+            this.btnWeight.BackgroundImage = global::MainUnit.Properties.Resources.time;
+            this.btnWeight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnWeight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnWeight.Location = new System.Drawing.Point(103, 113);
+            this.btnWeight.Name = "btnWeight";
+            this.btnWeight.Size = new System.Drawing.Size(75, 72);
+            this.btnWeight.TabIndex = 5;
+            this.btnWeight.UseVisualStyleBackColor = true;
+            this.btnWeight.UseWaitCursor = true;
+            this.btnWeight.Click += new System.EventHandler(this.btnWeight_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::MainUnit.Properties.Resources.angle;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(57, 158);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(82, 77);
+            this.button4.TabIndex = 8;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseWaitCursor = true;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::MainUnit.Properties.Resources.weight;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(146, 74);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(82, 77);
+            this.button3.TabIndex = 7;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseWaitCursor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::MainUnit.Properties.Resources.temp__2_;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(57, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 77);
+            this.button2.TabIndex = 6;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseWaitCursor = true;
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = global::MainUnit.Properties.Resources.lenght;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(145, 158);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(82, 77);
+            this.button5.TabIndex = 9;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.UseWaitCursor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(293, 277);
+            this.Controls.Add(this.btnHome);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnWeight);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Unit Converter";
+            this.UseWaitCursor = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWeight;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnHome;
+    }
+}
+
---
+++ MainUnit/MainUnit/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainUnit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnWeight.Region = new Region(new System.Drawing.Drawing2D.GraphicsPath());
+            var pathRed = new System.Drawing.Drawing2D.GraphicsPath();
+            pathRed.AddEllipse(0, 0, btnWeight.Width, btnWeight.Height);
+            btnWeight.Region = new Region(pathRed);
+
+            panel1.Region = new Region(new System.Drawing.Drawing2D.GraphicsPath());
+            var pathRed2 = new System.Drawing.Drawing2D.GraphicsPath();
+            pathRed2.AddEllipse(0, 0, panel1.Width, panel1.Height);
+            panel1.Region = new Region(pathRed2);
+
+            
+        }
+
+        private void btnWeight_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
